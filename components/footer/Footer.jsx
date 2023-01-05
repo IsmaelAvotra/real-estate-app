88b9f8454e976b684fa60dc23e9760b50f424368
Ismael avotrasoa <ismaelavotrasoa@Ismaels-MacBook-Pro.local>
2023-01-05 18:47:22 +0100

    correction footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import Link from 'next/link'
 
 const Footer = () => {
   const date = new Date().getFullYear()
   return (
     <footer className={styles.footer}>
       <p>
-        © created by <span>Ismael Avotra</span> {date} | All rights reserved
+        © created by{' '}
+        <Link href='https://portfolio-ismael.vercel.app/' target='_blank'>
+          <span>Ismael Avotra</span>
+        </Link>{' '}
+        {date} | All rights reserved
       </p>
     </footer>
   )
